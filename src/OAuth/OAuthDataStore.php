78055cb1adb00851f1adc49868ad0a15c07a1352
ronak16 <ronak.makadia@yahoo.com>
2017-08-03 10:33:41 +0530

    Update OAuthDataStore File As per requirement
    To verify OAuthSignature from lti request from canvas need require changes

--- src/OAuth/OAuthDataStore.php
+++ src/OAuth/OAuthDataStore.php
@@ -1,32 +1,38 @@
 <?php
 
-namespace IMSGlobal\LTI\OAuth;
-
 /**
  * Class to represent an %OAuth Data Store
  *
- * @copyright  Andy Smith
- * @version 2008-08-04
- * @license https://opensource.org/licenses/MIT The MIT License
  */
 class OAuthDataStore {
+
+    private $consumer_key = NULL;
+    private $consumer_secret = NULL;
+
+    public function __construct($consumer_key, $consumer_secret) {
+      $this->consumer_key = $consumer_key;
+      $this->consumer_secret = $consumer_secret;
+
+    }
+
     function lookup_consumer($consumer_key) {
-        // implement me
+         return new OAuthConsumer($this->consumer_key, $this->consumer_secret);
     }
 
     function lookup_token($consumer, $token_type, $token) {
-        // implement me
+        return new OAuthToken($consumer, '');
     }
 
     function lookup_nonce($consumer, $token, $nonce, $timestamp) {
-        // implement me
+       return FALSE;
     }
 
     function new_request_token($consumer, $callback = null) {
-        // return a new token attached to this consumer
+        return NULL;
     }
 
     function new_access_token($token, $consumer, $verifier = null) {
+         return NULL;
         // return a new access token attached to this consumer
         // for the user associated with this token if the request token
         // is authorized
